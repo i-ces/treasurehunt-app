d286460ab6c6e8dabe92b71d3495fe733af12afc
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-20 23:24:45 +0545

    add accordion code

--- lib/src/widgets/accordion.dart
+++ lib/src/widgets/accordion.dart
@@ -1,15 +1,129 @@
 import 'package:flutter/material.dart';
 
 class Accordion extends StatefulWidget {
-  const Accordion({super.key});
+  final String title;
+  final Widget content;
+
+  const Accordion({
+    super.key,
+    required this.title,
+    required this.content,
+  });
 
   @override
-  State<Accordion> createState() => _AccordionState();
+  _AccordionState createState() => _AccordionState();
 }
 
-class _AccordionState extends State<Accordion> {
+class _AccordionState extends State<Accordion>
+    with SingleTickerProviderStateMixin {
+  bool _isExpanded = false;
+  late AnimationController _controller;
+  late Animation<double> _heightFactor;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _heightFactor = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+  }
+
+  void _toggleExpand() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      if (_isExpanded) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: _toggleExpand,
+          child: AnimatedContainer(
+            width: MediaQuery.of(context).size.width * 0.8,
+            duration: const Duration(milliseconds: 300),
+            decoration: BoxDecoration(
+              color: Colors.orangeAccent,
+              borderRadius: _isExpanded
+                  ? const BorderRadius.vertical(top: Radius.circular(12))
+                  : BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+            ),
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    const SizedBox(width: 10),
+                    Text(
+                      widget.title,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                Icon(
+                  _isExpanded ? Icons.expand_less : Icons.expand_more,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+        ),
+        AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                bottom: Radius.circular(12),
+              ),
+              child: Align(
+                heightFactor: _heightFactor.value,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  decoration: BoxDecoration(
+                    color: Colors.orange.shade100,
+                    borderRadius: const BorderRadius.vertical(
+                      bottom: Radius.circular(12),
+                    ),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: widget.content,
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+    );
   }
 }
