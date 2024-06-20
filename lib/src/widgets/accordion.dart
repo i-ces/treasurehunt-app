9a89509a4b21e6373fefbb432b66667b181c9714
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-20 22:35:51 +0545

    add accordion widget init

---
+++ lib/src/widgets/accordion.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Accordion extends StatefulWidget {
+  const Accordion({super.key});
+
+  @override
+  State<Accordion> createState() => _AccordionState();
+}
+
+class _AccordionState extends State<Accordion> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
