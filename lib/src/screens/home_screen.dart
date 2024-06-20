69c85204dfc9155b0098ecce7c16fc464a126bb8
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-20 21:41:30 +0545

    create the final screen for splash

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:treasurehunt/src/screens/welcome_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('home screen ')));
+    return const WelcomeScreen();
   }
 }
---
+++ lib/src/screens/welcome_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:treasurehunt/src/widgets/custom_button.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/images/fluid.svg',
+                  width: MediaQuery.of(context).size.width * 0.8,
+                ),
+                SvgPicture.asset(
+                  'assets/images/logo-word.svg',
+                  width: MediaQuery.of(context).size.width * 0.5,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Welcome to Hack & Hunt!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color.fromRGBO(66, 65, 66, 1),
+                fontFamily: 'Poppins',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'A Mysterious Adventure for Those\nWho Speak the Language of Code',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color.fromRGBO(9, 15, 71, 0.5),
+                fontFamily: 'Poppins',
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            const SizedBox(height: 30),
+            CustomButton(text: "Get Started", onPressed: () {})
+          ],
+        ),
+      ),
+    );
+  }
+}
