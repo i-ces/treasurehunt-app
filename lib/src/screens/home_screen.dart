5a56c763c2e7c639f013f5c3002504b9b61b950c
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-20 09:53:48 +0545

    clean up may be i dunno what

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -5,6 +5,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('home screen ');
+    return const Scaffold(body: Center(child: Text('home screen ')));
   }
 }
