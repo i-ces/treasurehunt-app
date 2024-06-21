d389d613ad6bf105de9d00b13128fc9b74da453b
Seamoon Pandey <115852972+seamoonpandey@users.noreply.github.com>
2024-06-21 23:53:27 +0545

    [SP] HOTFIX Update custom button (#7)
    * update button
    update asset image path
    
    * dart fix

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -3,13 +3,12 @@ import 'package:treasurehunt/src/pages/home_page.dart';
 import 'package:treasurehunt/src/pages/leaderboard_page.dart';
 import 'package:treasurehunt/src/pages/riddles.dart';
 import 'package:treasurehunt/src/pages/sponsor.dart';
-import 'package:treasurehunt/src/widgets/accordion.dart';
 import 'package:treasurehunt/src/widgets/custom_app_bar.dart';
 import 'package:treasurehunt/src/widgets/nav_bar.dart';
 
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -25,16 +24,16 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   final List<Widget> _screens = [
-    HomePage(),
-    LeaderboardPage(),
-    RiddlesPage(),
-    SponsorPage(),
+    const HomePage(),
+    const LeaderboardPage(),
+    const RiddlesPage(),
+    const SponsorPage(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(),
+      appBar: const CustomAppBar(),
       body: _screens[_selectedIndex],
       bottomNavigationBar: NavBar(
         onItemSelected: _onItemTapped,
