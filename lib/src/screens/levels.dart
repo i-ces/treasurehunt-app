68075a1e6b0f92c3ba52d4892ba026cbe2f1874b
dhirajchaurasiya10 <106879418+dhirajchaurasiya10@users.noreply.github.com>
2024-06-22 22:05:22 +0545

    creates levelspage (#10)

---
+++ lib/src/screens/levels.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:treasurehunt/src/utils/colors.dart';
+import 'package:treasurehunt/src/widgets/custom_button.dart';
+import 'package:treasurehunt/src/widgets/dallo.dart';
+
+class LevelsPage extends StatefulWidget {
+  const LevelsPage({super.key});
+
+  @override
+  State<LevelsPage> createState() => _LevelsPageState();
+}
+
+class _LevelsPageState extends State<LevelsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Dallo(name: 'Riddle #001'),
+            SizedBox(height: 20),
+            Text(
+              "Level-01",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ), // Replace this with the riddles id from api
+            Divider(color: AppColors.AccentColor),
+            Text(
+              "To start a journey full of fun,\nFind the place where news is spun.\nLook for the latest club's delight,\nOn platforms where we share our sight.\nSeek the post that starts the quest,\nFollow the link, be our guest.",
+              style: TextStyle(color: AppColors.TextColor2, fontSize: 18),
+            ),
+            Divider(color: AppColors.AccentColor),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    // Background image
+                    FractionallySizedBox(
+                      widthFactor: 0.8, // Adjust the width factor as needed
+                      child: Container(
+                        width: double.infinity, // Make the container take full width
+                        height: 150, // Adjust the height as needed
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/logo-blue.png'),
+                            fit: BoxFit.contain,
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                    // TextFormField
+                    Container(
+                      width: 300, // Adjust the width as needed
+                      height: 200, // Adjust the height as needed
+                      padding: EdgeInsets.all(8.0), // Adjust the padding as needed
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 29, 27, 27).withOpacity(0.9), // Background color
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: TextFormField(
+                        // controller: _controller,
+                        maxLines: null, // Allow multiple lines
+                        decoration: InputDecoration(
+                          hintText: 'Write your answer.....',
+                          hintStyle: TextStyle(
+                            color: Colors.white.withOpacity(0.6), // Adjust label text color as needed
+                          ),
+                          border: InputBorder.none, // Remove border
+                        ),
+                        style: TextStyle(
+                          color: Colors.white, // Adjust text color as needed
+                        ),
+                        cursorColor: Colors.blue, // Adjust cursor color as needed
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            CustomButton(text: 'Submit', onPressed: () {}),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() => runApp(MaterialApp(home: LevelsPage()));
