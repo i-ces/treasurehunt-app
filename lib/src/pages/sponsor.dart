e2ac9a9023c67a77c1feaa6d0b396ac462fff300
Upendra Raj Joshi <upendraofficial10@gmail.com>
2024-06-24 00:16:45 +0545

    Finalized Sponsors, Riddles

--- lib/src/pages/sponsor.dart
+++ lib/src/pages/sponsor.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:treasurehunt/src/utils/sponsors_data.dart';
+import 'package:treasurehunt/src/widgets/accordion.dart';
+import 'package:treasurehunt/src/widgets/dallo.dart';
+import 'package:treasurehunt/src/widgets/sponsor_card.dart';
 
-class SponsorPage extends StatelessWidget {
+class SponsorsPage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Sponsor', style: TextStyle(fontSize: 24)),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Dallo(name: "Get to know our sponsors!", onBackPressed: (){}, ),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemCount: sponsors.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(bottom: 16.0),
+                  child: Accordion(
+                    sponsorType: sponsors[index]['type']!,
+                    sponsorName: sponsors[index]['name']!,
+                    sponsorImage: sponsors[index]['image']!,
+                    sponsorDescription: sponsors[index]['description']!,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
