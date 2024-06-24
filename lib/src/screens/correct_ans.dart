083cd390373ec783d7dd841f35291264e15dd53f
dhirajchaurasiya10 <106879418+dhirajchaurasiya10@users.noreply.github.com>
2024-06-24 21:52:04 +0545

    Build UI for correct answer screen (#13)
    * adds correct ans? screen
    
    * managed select case

---
+++ lib/src/screens/correct_ans.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:treasurehunt/src/utils/colors.dart';
+import 'package:treasurehunt/src/widgets/custom_button.dart';
+import 'package:treasurehunt/src/widgets/dallo.dart';
+
+class CorrectAns extends StatefulWidget {
+  final int displayContent;
+
+  const CorrectAns({super.key, required this.displayContent});
+
+  @override
+  State<CorrectAns> createState() => _CorrectAnsState();
+}
+
+class _CorrectAnsState extends State<CorrectAns> {
+  @override
+  Widget build(BuildContext context) {
+    String text;
+    String subtext;
+    String imagePath;
+
+    switch (widget.displayContent) {
+      case 0:
+        text = "Congratulations!";
+        imagePath = 'assets/images/Tick.png';
+        subtext = "You’re one step closure to finding \n the whereabouts of Mr. Umi";
+        break;
+      case 1:
+        text = "Oh no!";
+        imagePath = 'assets/images/cross.png'; 
+        subtext = "Looks like you made an oopsie! Please check\n the riddles carefully and answer again!";
+        break;
+      case 2:
+        text = "Danger!";
+        imagePath = 'assets/images/trap.png'; 
+        subtext = "Looks like you’ve encountered a trap! Please move a step back!";
+        break;
+      default:
+        text = "Danger!";
+        imagePath = 'assets/images/trap.png'; 
+        subtext = "Looks like you’ve encountered a trap! Please move a step back!";
+        break;
+    }
+
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        const Align(
+          alignment: Alignment.topCenter,
+          child: Dallo(name: 'Riddle #01'),),      //replace this with the riddle id
+        Align(
+          alignment: Alignment.centerRight,
+          child: Image.asset(
+            'assets/images/Logo-Owl-Only.png',
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(imagePath),
+              SizedBox(height: 30),
+              Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 28,
+                  color: AppColors.MainColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                subtext,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Color.fromARGB(255, 89, 118, 167),
+                ),
+              ),
+              SizedBox(height: 30),
+              CustomButton(text: 'Next', onPressed: () {}),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
