84cc2748cb092b9ee74d289248f3ab2824fb70d1
Anisha Bhandari <120290735+anishabhandari55@users.noreply.github.com>
2024-06-27 03:08:02 +0545

    Anisha (#18)
    * add leaderboard and home page
    
    * readd leaderboard and home page
    
    * clean up the code
    
    * fix leaderboard ui
    
    ---------
    
    Co-authored-by: Seamoon Pandey <hackedasocial@gmail.com>

--- lib/src/widgets/floating_bar.dart
+++ lib/src/widgets/floating_bar.dart
@@ -3,40 +3,71 @@ import 'package:flutter/material.dart';
 class FloatingBar extends StatelessWidget {
   final String name;
   final bool showBackButton;
-  final void Function() onBackPressed;
+  final bool contestant;
+  final String position;
+  final bool showElevation;
+  final Color dalloColor;
+  final Color dalloContentColor;
+  final void Function()? onBackPressed;
 
   const FloatingBar({
     required this.name,
     this.showBackButton = false,
-    required this.onBackPressed,
+    this.contestant = false,
+    this.position = '',
+    this.showElevation = true,
+    this.dalloColor = const Color.fromRGBO(255, 255, 255, 1),
+    this.dalloContentColor = const Color.fromARGB(7, 0, 8, 14),
+    this.onBackPressed,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: dalloColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(48.0),
       ),
-      color: Colors.white,
-      elevation: 10,
+      elevation: showElevation ? 10 : 0,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Row(
           children: [
             if (showBackButton)
               IconButton(
-                  icon: const Icon(Icons.arrow_back),
-                  onPressed: () => {
-                        onBackPressed(),
-                      }
+                  icon: const Icon(Icons.arrow_back), onPressed: onBackPressed
+
                   // Navigator.of(context).pop(),
                   ),
+            if (contestant)
+              Expanded(
+                  child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        position,
+                        style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Poppins',
+                            color: Color(0xffffffff)),
+                      ))),
             Expanded(
-              child: Text(
-                name,
-                style: const TextStyle(fontSize: 20),
-                textAlign: showBackButton ? TextAlign.start : TextAlign.center,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  name,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'Poppins',
+                    color: dalloContentColor,
+                  ),
+                  // background: #090F4780;
+
+                  textAlign:
+                      showBackButton ? TextAlign.start : TextAlign.center,
+                ),
               ),
             ),
           ],
---
+++ lib/src/widgets/leaderboard_component.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class LeaderboardComponent extends StatelessWidget {
+  final String teamname;
+  final double? containerHeight;
+  final double? containerWidth;
+  final double? circleHeight;
+  final double? circleWidth;
+  final EdgeInsetsGeometry containerPaddingFromTop;
+  final double? smallCircleHeight;
+  final double? smallCircleWidth;
+  final double? positionSmallCircle;
+  final String smallCircleText;
+
+  const LeaderboardComponent(
+      {super.key,
+      required this.teamname,
+      this.containerHeight,
+      this.smallCircleText = '#1',
+      this.containerWidth,
+      this.circleHeight,
+      this.circleWidth,
+      this.containerPaddingFromTop = const EdgeInsets.only(top: 80),
+      this.positionSmallCircle,
+      this.smallCircleHeight,
+      this.smallCircleWidth});
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        // Image.asset('assets/images/random.png'),
+        Center(
+          child: Container(
+            margin: const EdgeInsets.only(top: 70),
+            height: containerHeight ?? 177,
+            width: containerWidth ?? 138,
+            decoration: const BoxDecoration(
+                color: Color(0xffE1E1E1),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0))),
+            child: Padding(
+              padding: containerPaddingFromTop,
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Text(teamname,
+                      style: const TextStyle(
+                          color: Color(0xff090F47),
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w500,
+                          fontSize: 21))),
+            ),
+          ),
+        ),
+        Center(
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                height: circleHeight ?? 138,
+                width: circleWidth ?? 138,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border:
+                        Border.all(color: const Color(0xffFBAE40), width: 8),
+                    image: const DecorationImage(
+                        image: AssetImage('assets/images/random.png'))),
+              ),
+              Positioned(
+                bottom: -20,
+                left: positionSmallCircle ?? 100 / 2,
+                child: Container(
+                  height: smallCircleHeight ?? 41,
+                  width: 41,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xffFBAE40),
+                  ),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      smallCircleText,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
