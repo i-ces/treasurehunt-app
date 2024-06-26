2857dc1b3320de2a5096851339c9e943b8718226
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-26 21:52:01 +0545

    remove unnecesary middleware and update get request

--- lib/src/utils/middleware/api.dart
+++ lib/src/utils/middleware/api.dart
@@ -4,39 +4,21 @@ import 'dart:convert';
 const String API_URL = "http://localhost:8080";
 
 class ApiMiddleware {
-  final String endpoint;
-
-  ApiMiddleware(this.endpoint);
-
   Future<http.Response> get(String path) async {
-    final url = Uri.parse('$API_URL$endpoint$path');
+    final url = Uri.parse('$API_URL$path');
     final response = await http.get(url);
     return response;
   }
 
   Future<http.Response> post(String path, Map<String, dynamic> data) async {
-    final url = Uri.parse('$API_URL$endpoint$path');
+    final url = Uri.parse('$API_URL$path');
     final response = await http.post(
       url,
-      headers: {'Content-Type': 'application/json'},
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body: jsonEncode(data),
     );
-    return response;
-  }
-
-  Future<http.Response> put(String path, Map<String, dynamic> data) async {
-    final url = Uri.parse('$API_URL$endpoint$path');
-    final response = await http.put(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(data),
-    );
-    return response;
-  }
-
-  Future<http.Response> delete(String path) async {
-    final url = Uri.parse('$API_URL$endpoint$path');
-    final response = await http.delete(url);
     return response;
   }
 }
