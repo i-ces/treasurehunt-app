dd1349d827bdae9d5712f707edc23aae9f5a47f6
Shishir Rijal <shishirrijal@Shishirs-Laptop.local>
2024-06-26 23:54:18 +0545

    add riddle level screen

--- lib/src/screens/selecting_numbers.dart
+++ lib/src/screens/selecting_numbers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:treasurehunt/src/screens/levels.dart';
 import 'package:treasurehunt/src/utils/colors.dart';
 import 'package:treasurehunt/src/widgets/custom_app_bar.dart';
 import 'package:treasurehunt/src/widgets/floating_bar.dart';
@@ -31,26 +32,93 @@ class RiddleDetailPage extends StatelessWidget {
             onBackPressed: () => Navigator.of(context).pop(),
           ),
           Expanded(
-            child: FutureBuilder<String>(
-              future: fetchRiddleDescription(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text('No description available'));
-                } else {
-                  return Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      snapshot.data!,
-                      style: const TextStyle(
-                          fontSize: 18, color: AppColors.TextColor1),
+            // child:
+            //  FutureBuilder<String>(
+            //   future: fetchRiddleDescription(),
+            //   builder: (context, snapshot) {
+            //     if (snapshot.connectionState == ConnectionState.waiting) {
+            //       return const Center(child: CircularProgressIndicator());
+            //     } else if (snapshot.hasError) {
+            //       return Center(child: Text('Error: ${snapshot.error}'));
+            //     } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            //       return const Center(child: Text('No description available'));
+            //     } else {
+            //       return Padding(
+            //         padding: const EdgeInsets.all(16.0),
+            //         child: Text(
+            //           snapshot.data!,
+            //           style: const TextStyle(
+            //               fontSize: 18, color: AppColors.TextColor1),
+            //         ),
+            //       );
+            //     }
+            //   },
+            // ),
+
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: const Text(
+                      "The Number’s Dilemma",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                  );
-                }
-              },
+                  ), // Replace this with the riddles id from api
+
+                  const Divider(color: AppColors.AccentColor),
+                  const SizedBox(height: 20),
+
+                  Expanded(
+                    child: GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 2,
+                          crossAxisSpacing: 20,
+                        ),
+                        itemCount: 10,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => const LevelsPage()));
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(bottom: 20),
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(30),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.2),
+                                    spreadRadius: 2,
+                                    blurRadius: 8,
+                                    offset: const Offset(0, 8),
+                                  ),
+                                ],
+                              ),
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 25),
+                              child: Text(
+                                'Riddle #${index + 1}',
+                                style: TextStyle(
+                                    color:
+                                        AppColors.TextColor2.withOpacity(0.5),
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          );
+                        }),
+                  )
+                ],
+              ),
             ),
           ),
         ],
