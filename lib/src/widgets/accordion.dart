beab7ad0f6073e5ff0cbb7f0a31da3d7265efdfc
Shishir Rijal <shishirrijal@Shishirs-Laptop.local>
2024-06-26 19:55:30 +0545

    sponsor fixes

--- lib/src/widgets/accordion.dart
+++ lib/src/widgets/accordion.dart
@@ -1,10 +1,9 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-
+import 'package:treasurehunt/src/utils/colors.dart';
 
 class Accordion extends StatefulWidget {
-
   final String sponsorType;
   final String sponsorName;
   final String sponsorImage;
@@ -65,7 +64,9 @@ class _AccordionState extends State<Accordion>
         GestureDetector(
           onTap: _toggleExpand,
           child: AnimatedContainer(
-            width: MediaQuery.of(context).size.width * 0.8,
+            // margin: EdgeInsets.symmetric(horizontal: 10),
+            width: double.infinity,
+            // width: MediaQuery.of(context).size.width * 0.8,
             height: 142,
             duration: const Duration(milliseconds: 300),
             decoration: BoxDecoration(
@@ -81,7 +82,7 @@ class _AccordionState extends State<Accordion>
                 ),
               ],
             ),
-            padding: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -91,17 +92,20 @@ class _AccordionState extends State<Accordion>
                       // Ensures square shape and clipped corners
                       borderRadius: BorderRadius.circular(12.0),
                       child: widget.sponsorImage.isNotEmpty
-                          ? Image.asset(widget.sponsorImage,
-                              width: 120,
+                          ? Image.asset(
+                              widget.sponsorImage,
+                              width: 100,
+                              height: 100,
                               fit: BoxFit.cover,
                             )
                           : SvgPicture.asset(
                               width: 100,
+                              height: 100,
                               'assets/logo.svg',
                               color: Colors.white,
                             ),
                     ),
-                    const SizedBox(width: 10),
+                    const SizedBox(width: 20),
                     Center(
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -115,20 +119,28 @@ class _AccordionState extends State<Accordion>
                               color: Colors.white,
                             ),
                             maxLines: 2,
-                            
                           ),
-                          Text(widget.sponsorType,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),),
-                                        ],),
+                          Text(
+                            widget.sponsorType,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
-                Icon(
-                  _isExpanded ? Icons.expand_less : Icons.expand_more,
-                  color: Colors.white,
+                Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: Icon(
+                    _isExpanded ? Icons.expand_less : Icons.expand_more,
+                    color: AppColors.AccentColor,
+                  ),
                 ),
               ],
             ),
@@ -145,7 +157,7 @@ class _AccordionState extends State<Accordion>
                 heightFactor: _heightFactor.value,
                 child: AnimatedContainer(
                   duration: const Duration(milliseconds: 300),
-                  width: MediaQuery.of(context).size.width * 0.8,
+                  width: double.infinity,
                   decoration: BoxDecoration(
                     color: Colors.orange.shade100,
                     borderRadius: const BorderRadius.vertical(
