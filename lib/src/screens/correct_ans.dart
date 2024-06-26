bf8cb6dd83946d759adbdfef225988a1b8e918bb
Shishir Rijal <shishirrijal@Shishirs-Laptop.local>
2024-06-27 00:24:51 +0545

    fix correct ans screen

--- lib/src/screens/correct_ans.dart
+++ lib/src/screens/correct_ans.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:treasurehunt/src/utils/colors.dart';
+import 'package:treasurehunt/src/widgets/custom_app_bar.dart';
 import 'package:treasurehunt/src/widgets/custom_button.dart';
-import 'package:treasurehunt/src/widgets/floating_bar.dart';
+
+enum Status { congratulations, ohno, danger }
 
 class CorrectAns extends StatefulWidget {
-  final int displayContent;
+  final Status status;
 
-  const CorrectAns({super.key, required this.displayContent});
+  const CorrectAns({super.key, required this.status});
 
   @override
   State<CorrectAns> createState() => _CorrectAnsState();
@@ -19,20 +21,20 @@ class _CorrectAnsState extends State<CorrectAns> {
     String subtext;
     String imagePath;
 
-    switch (widget.displayContent) {
-      case 0:
+    switch (widget.status) {
+      case Status.congratulations:
         text = "Congratulations!";
         imagePath = 'assets/images/Tick.png';
         subtext =
             "You’re one step closure to finding \n the whereabouts of Mr. Umi";
         break;
-      case 1:
+      case Status.ohno:
         text = "Oh no!";
         imagePath = 'assets/images/cross.png';
         subtext =
             "Looks like you made an oopsie! Please check\n the riddles carefully and answer again!";
         break;
-      case 2:
+      case Status.danger:
         text = "Danger!";
         imagePath = 'assets/images/trap.png';
         subtext =
@@ -46,52 +48,55 @@ class _CorrectAnsState extends State<CorrectAns> {
         break;
     }
 
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Align(
-          alignment: Alignment.topCenter,
-          child: FloatingBar(
-            name: 'Riddle #01',
-            onBackPressed: () {},
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          // Align(
+          //   alignment: Alignment.topCenter,
+          //   child: FloatingBar(
+          //     name: 'Riddle #01',
+          //     onBackPressed: () {},
+          //   ),
+          // ), //replace this with the riddle id
+          Align(
+            alignment: Alignment.centerRight,
+            child: Image.asset(
+              'assets/images/Logo-Owl-Only.png',
+            ),
           ),
-        ), //replace this with the riddle id
-        Align(
-          alignment: Alignment.centerRight,
-          child: Image.asset(
-            'assets/images/Logo-Owl-Only.png',
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(imagePath),
-              const SizedBox(height: 30),
-              Text(
-                text,
-                style: const TextStyle(
-                  fontSize: 28,
-                  color: AppColors.MainColor,
-                  fontWeight: FontWeight.bold,
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(imagePath),
+                const SizedBox(height: 30),
+                Text(
+                  text,
+                  style: const TextStyle(
+                    fontSize: 28,
+                    color: AppColors.MainColor,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 12),
-              Text(
-                subtext,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 14,
-                  color: Color.fromARGB(255, 89, 118, 167),
+                const SizedBox(height: 12),
+                Text(
+                  subtext,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 89, 118, 167),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              CustomButton(text: 'Next', onPressed: () {}),
-            ],
+                const SizedBox(height: 30),
+                CustomButton(text: 'Next', onPressed: () {}),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
